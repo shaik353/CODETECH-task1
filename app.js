a1376ff4d3d30431a1ad8183820d539209570975
shaik353 <sksamee1919@gmail.com>
2024-12-28 22:37:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
