d9859808e5aacbb725149b4a82d795e26f16a0c0
shaik353 <sksamee1919@gmail.com>
2024-12-28 22:35:00 +0530

    Create Todolist.js

---
+++ Todolist.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (text) => {
+    const newTask = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const toggleComplete = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div>
+      <TodoForm addTask={addTask} />
+      {tasks.map((task) => (
+        <TodoItem
+          key={task.id}
+          task={task}
+          toggleComplete={toggleComplete}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
